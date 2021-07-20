952e47a80ceada00190675d497c08b93726a5f5f
Thanh hau <Thanhhau9979@gmail.com>
2021-07-20 10:02:01 +0700

    da commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,4 +28,64 @@ window.addEventListener("click", function (e) {
     }
 });
 //accordion menu
+// $(document).ready(function(){
+//   $(".wedo-item-header").click(function(){
+//     $(".wedo-item").removeClass("active");    
+//     $(this).parent().addClass("active");
+//     $(this).parent().slideUp();
+//     $(this).next().slideToggle();
+//   });
+// });
+$(document).ready(function() {
+  $(".wedo-item-header").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".wedo-item-info")
+        .slideUp(400);
+      $(".wedo-item-arrow")
+        .removeClass("fa-angle-up")
+        .addClass("fa-angle-down");
+    } else {
+      $(".wedo-item-arrow")
+        .removeClass("fa-angle-up")
+        .addClass("fa-angle-down");
+      $(this)
+        .find("i")
+        .removeClass("fa-angle-down")
+        .addClass("fa-angle-up");
+      $(".wedo-item-header").removeClass("active");
+      $(this).addClass("active");
+      $(".wedo-item-info").slideUp(400);
+      $(this)
+        .siblings(".wedo-item-info")
+        .slideDown(400);
+    }
+  });
+});
+// Button back to top
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 500) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+//toggle search bar
+$(document).ready(function() {
+     
+  $(".fa-search").click(function() {
+     $(".search-box").slideToggle();
+     $("input[type='text']").focus();
+   });
+
+});
+
 
